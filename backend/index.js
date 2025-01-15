cd37373cd1b9dffc0a42fc91099b4a5351d46746
Suman Kumar <sumanqaj9876@gmail.com>
2025-01-15 11:56:44 +0530

    enhance server setup with CORS, improved logging, and default route

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,30 @@ const port = 9000;
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const bookRoute = require("./routes/bookRoutes");
+
+// Database connection
 mongoose.connect("mongodb://127.0.0.1:27017/bookmanagement").then(() => {
   console.log("DB Connected!!!");
 });
+
+// Allow all origins with CORS
 app.use(cors());
+
+// Body parser middleware
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-const UserRoute = require("./routes/UserRoute")
+
+// Routes
+const UserRoute = require("./routes/UserRoute");
 app.use("/books", bookRoute);
-app.use("/user", UserRoute)
+app.use("/user", UserRoute);
+
+// Default route
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Server run on ${port}`);
+  console.log(`Server running on ${port}`);
 });
